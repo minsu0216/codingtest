df2185be461b44ebf9a3464c01b8709688adaa79
Kang Minsu <86918832+minsu0216@users.noreply.github.com>
2022-03-16 09:29:05 +0000

    commit

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-'''
-itertools
-반복되는 데이터를 처리하는 기능
-순열, 조합, 중복 순열, 중복 조합
-'''
+# input = [['a', 'b'], ['b', 'c'], ['d', 'e'], ['b', 'd']]
+# output = ['a', 'c']
 
-from itertools import permutations
-from itertools import combinations
-from itertools import product
-from itertools import combinations_with_replacement
+import ctypes
 
-data = ['A', 'B', 'C']
-result1 = list(permutations(data, 2))
-result2 = list(combinations(data, 2))
-result3 = list(product(data, repeat=2))
-result4 = list(combinations_with_replacement(data, 2))
+a = [1, 2, 3]
 
-print(result1)
-print(result2) 
-print(result3)
-print(result4)
+oi1 = id(a)
+print(oi1)
+
+a = [2, 3, 4]
+# oi2 = id(a)
+# print(id(a))
+
+def get_object_by_id(object_id):
+    obj = ctypes.cast(object_id, ctypes.py_object).value
+    
+    return obj
+
+c = get_object_by_id(oi1)
+print(c[0])
